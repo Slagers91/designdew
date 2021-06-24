d49dd6597369573199911a746ff9c8a02db61f7a
Ruud Slagers <ruudslagers@gmail.com>
2021-06-24 16:49:43 +0200

    werkwijze updated

--- page-home.php
+++ page-home.php
@@ -2,23 +2,23 @@
 <?php get_header(); ?>
 
 <!------------------------ HERO ------------------------->
-<section class="hero">
-	<div class="container">
+<section class="hero"><!--HERO-->
+	<div class="container"><!--CONTAINER-->
 		<h1>Brengt ideeën tot leven en laat ze groeien op het web.</h1>
 		<button onclick="location.href='#projecten'" type="button">Bekijk projecten</button>
 
-		<div class="hero-img">
+		<div class="hero-img"><!--HERO-IMG-->
 			<img src="<?php THEMEDIR; ?>/wp-content/themes/designdew/dist/images/iPhone X.png" alt="">
-		</div>
-	</div>
-</section>
+		</div><!--HERO-IMG-->
+	</div><!--CONTAINER-->
+</section><!--HERO-->
 
 <!-------------------- ONZE DIENSTEN -------------------->
-<section class="onze-diensten">
-	<div class="container">
+<section class="onze-diensten"><!--ONZE-DIENSTEN-->
+	<div class="container"><!--CONTAINER-->
 		<h2>Onze Diensten</h2>
-		<div class="row">
-			<div class="col-sm">
+		<div class="row"><!--ROW-->
+			<div class="col-sm"><!--COL-SM-1-->
 				<div class="avatar">
 				<img src="/wp-content/themes/designdew/dist/images/Ruud-Slagers.png" alt="">
 				</div>
@@ -27,8 +27,8 @@
 					<p>Wij ontwerpen en bouwen goed uitziende en prettig werkende websites die bijdragen aan zakelijk succes. Centraal hierbij staan het doel, de uitstraling en de boodschap van jouw organisatie.</p>
 					<a src="#">Kom meer te weten</a>
 				</div>
-			</div>
-			<div class="col-sm">
+			</div><!--COL-SM-1-->
+			<div class="col-sm"><!--COL-SM-2-->
 				<div class="avatar">
 				<img src="/wp-content/themes/designdew/dist/images/Ruud-Slagers.png" alt="">
 				</div>
@@ -37,8 +37,8 @@
 					<p>Wij ontwerpen aantrekkelijke webshops. De vormgeving ervan wekt vertrouwen en is inspirerend; de manier van navigeren is juist simpel. Zo helpen we jouw bezoekers om snel en eenvoudig de juiste informatie te vinden en neemt de kans op een bestelling toe.</p>
 					<a src="#">Kom meer te weten</a>
 				</div>
-			</div>
-			<div class="col-sm">
+			</div><!--COL-SM-2-->
+			<div class="col-sm"><!--COL-SM-3-->
 			<div class="avatar">
 			<img src="/wp-content/themes/designdew/dist/images/Ruud-Slagers.png" alt="">
 			</div>
@@ -47,10 +47,10 @@
 					<p>Zoekmachineoptimalisatie (SEO) is een doorlopend proces, want er zijn altijd nieuwe potentiële kansen en veranderingen in de markt. Met een op maat gemaakte SEO strategie brengen wij jouw website naar organische topposities.</p>
 					<a src="#">Kom meer te weten</a>
 				</div>
-			</div>
-		</div>
-	</div>
-</section>
+			</div><!--COL-SM-3-->
+		</div><!--ROW-->
+	</div><!--CONTAINER-->
+</section><!--ONZE-DIENSTEN-->
 
 <!-------------------- RECENTE PROJECTEN -------------------->
 <section id="projecten" class="recente-projecten"><!--RECENTE PROJECTEN-->
@@ -100,84 +100,90 @@
 </section><!--RECENTE PROJECTEN-->
 
 <!-------------------- STATISTIEKEN -------------------->
-<section class="statistieken">
-	<div class="container">
-	<h2>Statistieken die er toe doen</h2>
-	<p class="sub-text">Wat designdew al bereikt heeft.</p>
-	<div class="row">
-			<div class="col-sm">
+<section class="statistieken"><!--STATISTIEKEN-->
+	<div class="container"><!--CONTAINER-->
+		<h2>Statistieken die er toe doen</h2>
+		<p class="sub-text">Wat designdew al bereikt heeft.</p>
+	<div class="row"><!--ROW-->
+			<div class="col-sm"><!--COL-SM-1-->
 				<h2>10+</h2>
 				<p class="sub-text">Websites/webshops</p>
-			</div>
-			<div class="col-sm">
+			</div><!--COL-SM-1-->
+			<div class="col-sm"><!--COL-SM-2-->
 			<h2>10+</h2>
 				<p class="sub-text">Tevreden klanten</p>
-			</div>
-			<div class="col-sm">
+			</div><!--COL-SM-2-->
+			<div class="col-sm"><!--COL-SM-3-->
 			<h2>4x</h2>
 				<p class="sub-text">De gehele website vernieuwd</p>
-			</div>
-			<div class="col-sm">
+			</div><!--COL-SM-3-->
+			<div class="col-sm"><!--COL-SM-4-->
 			<h2>3</h2>
 				<p class="sub-text">Jaar bestaan we</p>
-			</div>
-		</div>
-
-	</div>
-</section>
+			</div><!--COL-SM-4-->
+		</div><!--ROW-->
+	</div><!--CONTAINER-->
+</section><!--STATISTIEKEN-->
 
 <!-------------------- ONZE WERKWIJZE -------------------->
-<section class="werkwijze">
-	<div class="container">
+<section class="werkwijze"><!--WERKWIJZE-->
+	<div class="container"><!--CONTAINER-->
 	<h2>Onze werkwijze</h2>
-	<div class="row">
-			<div class="col-sm">
+	<div class="row"><!--ROW-->
+			<div class="col-sm"><!--COL-SM-1-->
 				<div class="ronde-lijn">
 				<div class="ronde">
-					<div class="lijn"></div>
 				</div>
+				<div class="lijn"></div>
 				</div>
 				<h6>Ontmoeting</h6>
 				<p class="sub-text">Vrijblijvend en efficiënt. Stellen samen de doelen en wensen op.</p>
-			</div>
-			<div class="col-sm">
-			<div class="ronde"></div>
+			</div><!--COL-SM-1-->
+			<div class="col-sm"><!--COL-SM-2-->
+			<div class="ronde-lijn">
+				<div class="ronde">
+				</div>
+				<div class="lijn"></div>
+				</div>
 			<h6>Design</h6>
 				<p class="sub-text">Wij gaan aan de slag om een passend ontwerp te maken. </p>
-			</div>
-			<div class="col-sm">
-			<div class="ronde"></div>
+			</div><!--COL-SM-2-->
+			<div class="col-sm"><!--COL-SM-3-->
+			<div class="ronde-lijn">
+				<div class="ronde">
+				</div>
+				<div class="lijn"></div>
+				</div>
 			<h6>Development</h6>
 				<p class="sub-text">Design goedgekeurd? Dan gaan wij het omzetten naar een werkend product.</p>
-			</div>
-			<div class="col-sm">
+			</div><!--COL-SM-3-->
+			<div class="col-sm"><!--COL-SM-4-->
 			<div class="ronde"></div>
 			<h6>Live</h6>
 				<p class="sub-text">Na de feedback rondes helemaal tevreden? Dan gaan we de website live zetten.</p>
-			</div>
-		</div>
-
-	</div>
-</section>
+			</div><!--COL-SM-4-->
+		</div><!--ROW-->
+	</div><!--CONTAINER-->
+</section><!--WERKWIJZE-->
 
 <!-------------------- BLOG BERICHTEN -------------------->
-<section class="blog">
-	<div class="container">
-	<div class="row">
-			<div class="col-sm">
+<section class="blog"><!--BLOG-->
+	<div class="container"><!--CONTAINER-->
+	<div class="row"><!--ROW-1-->
+			<div class="col-sm"><!--COL-SM-1-->
 			<h2>De voordelen van een website</h2>
 			</div>
 			<div class="col-sm">
 				<p class="sub-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Venenatis scelerisque at quam congue posuere libero in sit quam. Consequat, scelerisque non tincidunt sit lectus senectus.</p>
-			</div>
-		</div>
-		<div class="row">
+			</div><!--COL-SM-1-->
+		</div><!--ROW-1-->
+		<div class="row"><!--ROW-2-->
 		<div class="col-sm featured-img">
 		<img src="<?php THEMEDIR; ?>/wp-content/themes/designdew/dist/images/Cover-Voordelen-van-een-website-Blog.png" alt="">
 		</div>
-		</div>
-	</div>
-</section>
+		</div><!--ROW-2-->
+	</div><!--CONTAINER-->
+</section><!--BLOG-->
 
 <?php
 
